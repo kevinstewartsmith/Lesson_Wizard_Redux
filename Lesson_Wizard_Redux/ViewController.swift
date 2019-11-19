098c43a4de828ac3dec8034424e554dd84ab07c3
kssmith9 <kssmith9@ncsu.edu>
2019-11-19 14:29:59 +0700

    Initial Commit

---
+++ Lesson_Wizard_Redux/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lesson_Wizard_Redux
+//
+//  Created by Kevin Smith on 11/19/19.
+//  Copyright © 2019 Kevin Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
