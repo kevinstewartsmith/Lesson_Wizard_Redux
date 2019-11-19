5149e267933988d10184690ac9a03aa22b17f0a1
kssmith9 <kssmith9@ncsu.edu>
2019-11-19 14:42:38 +0700

    Add first VC UI elements.

--- Lesson_Wizard_Redux/ViewController.swift
+++ Lesson_Wizard_Redux/ViewController.swift
@@ -10,10 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var wordsOnDisplay: UITextView!
+    
+    
+    @IBOutlet weak var textBox: UITextField!
+    
+    
+    @IBAction func addWordsPressed(_ sender: UIButton) {
+    }
+   
+    
+    @IBAction func scramblePressed(_ sender: UIButton) {
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
 
 
 }
